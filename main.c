5f38816f4faa684a27c6890aaf116ad0e1b9a556
Raoni Silva <raoni.silvestre@hotmail.com>
2023-11-27 09:47:33 -0300

    trying to create some tables and auth

--- main.c
+++ main.c
@@ -1,6 +1,89 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void readTables()
+{
+  char message[100];
+  FILE *inputs;
+  inputs = fopen("./heading/tables.txt", "rb");
+  if (inputs == NULL)
+  {
+    printf("Error opening file!\n");
+    return;
+  }
+  while (fgets(message, 100, inputs) != NULL)
+  {
+    printf("%s", message);
+  }
+  fclose(inputs);
+}
+
+void createTable(char *tableName, char *columns)
+{
+  int flag = 0;
+
+  FILE *table;
+  char path[100];
+  strcpy(path, "./tables/");
+  strcat(path, tableName);
+  strcat(path, ".txt");
+  table = fopen(path, "wb");
+  if (table == NULL)
+  {
+    printf("Error opening file!\n");
+    return;
+  }
+  fclose(table);
+}
+
+int countLines(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return -1;
+    }
+    int count = 0;
+    char ch;
+    while((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            count++;
+        }
+    }
+
+    fclose(file);
+
+    return count;
+}
 
 int main(int argc, char *argv[])
 {
+  FILE *heading;
+  heading = fopen("./heading/tables.txt", "a");
+  if (heading == NULL)
+  {
+    printf("Error opening file!\n");
+    return 1;
+  }
   
+  char message[100];
+
+  while (1)
+  {
+    scanf("%s", message);
+    if (strcmp(message, "-1") == 0)
+    {
+      break;
+    }
+    fprintf(heading, "%s\n", message);
+  }
+
+  
+  fclose(heading);
+  int lines = countLines("./heading/tables.txt");
+  printf("\nQuantidade de linhas: %d\n", lines);
+
+  readTables();
+
+  return 0;
 }
