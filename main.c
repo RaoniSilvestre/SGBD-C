41daa2cf2dffb25317bbd6d402bd5620fb24d2ae
Raoni Silva <raoni.silvestre@hotmail.com>
2023-11-28 17:46:59 -0300

    Refactor main.c menu options

--- main.c
+++ main.c
@@ -2,19 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "./utils/createTable/createTable.h"
-#include "./utils/countLines/countLines.h"
 #include "./utils/openMenu/openMenu.h"
 
 #include "./functions/criarNovaTabela/criarNovaTabela.h"
 #include "./functions/lerTabelas/lerTabelas.h"
 
-
+// A main funcionará como um menu para acessar as funcionalidades do SGBD.
+// Cada funcionalidade será um arquivo .c separado dentro da pasta functions(São funções principais)
+// e será chamado de acordo com a escolha do usuário
 int main(int argc, char *argv[])
 {
-  // A main funcionará como um menu para acessar as funcionalidades do SGBD.
-  // Cada funcionalidade será um arquivo .c separado dentro da pasta functions(São funções principais) 
-  // e será chamado de acordo com a escolha do usuário
   int choice;
   while (1)
   {
@@ -26,47 +23,44 @@ int main(int argc, char *argv[])
     {
 
     case 1:
-      //Acesse a função criarNovaTabela() no arquivo functions/criarNovaTabela/criarNovaTabela.c
-      //para entender o que está acontecendo aqui.
+      // Acesse a função criarNovaTabela() no arquivo functions/criarNovaTabela/criarNovaTabela.c
+      // para entender o que está acontecendo aqui.
       criarNovaTabela();
       break;
 
-
     case 2:
-      //O mesmo é análogo para todos os outros casos. (Quando tiver terminado de fazer esse código)
+      // O mesmo é análogo para todos os outros casos. (Quando tiver terminado de fazer esse código)
       printf("Tabelas disponíveis: \n");
       lerTabelas();
       break;
-
 
     case 3:
       printf("Inserir dados\n");
       break;
 
-
     case 4:
       printf("Listar todos os dados de uma tabela\n");
       break;
-
 
     case 5:
       printf("Apagar registro\n");
       break;
 
-
     case 6:
       printf("Apagar tabela\n");
       break;
 
-
     case 7:
+      printf("Clear prompt\n");
+      system("clear");
+      break;
+    case 8:
       printf("Sair\n");
-      return 0; //Encerra o programa
+      return 0; // Encerra o programa
       break;
 
-
     default:
-      printf("Opção inválida\n"); //Nada acontece, reinicia o loop
+      printf("Opção inválida\n"); // Nada acontece, reinicia o loop
       break;
     }
   }
