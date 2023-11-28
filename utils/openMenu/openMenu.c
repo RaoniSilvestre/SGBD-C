6d1ccc95977a44cfd1126aa78c34a14fc905148f
Raoni Silva <raoni.silvestre@hotmail.com>
2023-11-28 17:46:21 -0300

    Update openMenu options

--- utils/openMenu/openMenu.c
+++ utils/openMenu/openMenu.c
@@ -13,7 +13,8 @@ void openMenu()
   printf("4. Listar todos os dados de uma tabela\n");
   printf("5. Apagar registro\n");
   printf("6. Apagar tabela\n");
-  printf("7. Sair\n\n");
+  printf("7. Limpar prompt\n");;
+  printf("8. Sair\n\n");
   printf("---------------------------\n");
   printf("Escolha: ");
 }
