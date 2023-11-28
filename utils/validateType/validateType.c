176e7e73a3e996691662e162dd5e15296695f4f3
Raoni Silva <raoni.silvestre@hotmail.com>
2023-11-28 17:46:39 -0300

    Add validateType function and header file

---
+++ utils/validateType/validateType.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "validateType.h"
+
+validateType(char *type)
+{
+  if (strcmp(type, "INT") == 0)
+  {
+    return 1;
+  }
+  else if (strcmp(type, "FLOAT") == 0)
+  {
+    return 1;
+  }
+  else if (strcmp(type, "CHAR") == 0)
+  {
+    return 1;
+  }
+  else if (strcmp(type, "STRING") == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
---
+++ utils/validateType/validateType.h
@@ -0,0 +1,7 @@
+// validateType.h
+#ifndef VALIDATE_TYPE_H
+#define VALIDATE_TYPE_H
+
+int validateType(char *type);
+
+#endif // VALIDATE_TYPE_H
