2c8eb8c270e17f46e83f55621f90364c94c1ebdc
Raoni Silva <raoni.silvestre@hotmail.com>
2023-11-28 17:45:43 -0300

    Refactor table creation function

--- functions/criarNovaTabela/criarNovaTabela.c
+++ functions/criarNovaTabela/criarNovaTabela.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "criarNovaTabela.h"
+
 #include "../../utils/createTable/createTable.h"
 #include "../../utils/countLines/countLines.h"
+#include "../../utils/validateType/validateType.h"
+#include "../../utils/verifyTablesExists/verifyTablesExists.h"
+
 void criarNovaTabela()
 {
-
-  int lines = countLines("./heading/tables.txt"); // Conta a quantidade de linhas no arquivo tables.txt
-
   printf("1. Selecionado.\n"); // Grande parte dos printfs são para entendimento do usuário
   printf("Insira o nome da tabela: ");
 
@@ -16,8 +18,8 @@ void criarNovaTabela()
   scanf("%s", newTableName);
 
   // O arquivo heading serve para listar todas as tabelas existentes
-  FILE *heading;                                // Cria um ponteiro para o arquivo tables.txt
-  heading = fopen("./heading/tables.txt", "a"); // Abre o arquivo tables.txt em modo de escrita (append)
+  FILE *heading;
+  heading = fopen("./heading/tables.txt", "a");
 
   if (heading == NULL)
   {
@@ -25,40 +27,65 @@ void criarNovaTabela()
     return;
   }
 
-  if (verifyTableExists(&lines, newTableName) == 1) //Verifica se a tabela já existe
+  if (verifyTableExists(newTableName) == 1) // Verifica se a tabela já existe
   {
-    printf("Tabela já existe\n"); 
+    printf("---------------------------\n");
+    printf("Erro: Tabela já existe!\n");
     return;
   }
 
-  else //Se não existir, cria a tabela
+  else // Se não existir, cria a tabela
   {
     printf("Quantidade de colunas: ");
-    
+
     int columns;
     scanf("%d", &columns);
-    
+    printf("---------------------------\n");
+    printf("Tipos de colunas disponíveis:\n");
+    printf("-----\n");
+    printf("INT\n");
+    printf("-----\n");
+    printf("FLOAT\n");
+    printf("-----\n");
+    printf("CHAR\n");
+    printf("-----\n");
+    printf("STRING\n");
+
     char columnType[30]; // Variável que armazenará o tipo da coluna
     char columnName[30]; // Variável que armazenará o nome da coluna
 
     char resultNameLine[(columns * 30) + columns - 1];
     memset(resultNameLine, 0, sizeof(resultNameLine)); // Limpa a variável resultNameLine
-    
+
     char resultTypeLine[(columns * 30) + columns - 1];
     memset(resultTypeLine, 0, sizeof(resultTypeLine)); // Limpa a variável resultTypeLine
 
     for (int i = 0; i < columns; i++) // Loop para inserir o nome e o tipo de cada coluna
     {
-      printf("---------------------------\n");
-      printf("Coluna %d\n", i + 1);
+      do
+      {
+        printf("---------------------------\n");
+        printf("Coluna %d\n", i + 1);
 
-      printf("Nome: ");
-      scanf("%s", columnName);
-      strcat(resultNameLine, columnName);
+        printf("Nome: ");
+        scanf("%s", columnName);
 
-      printf("Tipo: ");
-      scanf("%s", columnType);
-      strcat(resultTypeLine, columnType);
+        printf("Tipo: ");
+        scanf("%s", columnType);
+
+        if (validateType(columnType) == 0) // Verifica se o tipo inserido é válido
+        {
+          printf("Nome inválido\n");
+          return;
+        }
+        else // Adiciona o nome e o tipo da coluna na variável result
+        {
+          strcat(resultNameLine, columnName);
+          strcat(resultTypeLine, columnType);
+          break;
+        }
+        /* code here */
+      } while (1);
 
       if (i < columns - 1)
       {
@@ -67,10 +94,9 @@ void criarNovaTabela()
       }
     }
 
-    //Optei por fazer essa createTable pois o código já estava muito extenso para uma função só.
-    //Além disso, essa createTable se preocupa apenas em colocar os dados (caso sejam validados nessa função) no arquivo tables.txt
+    // Optei por fazer essa createTable pois o código já estava muito extenso para uma função só.
+    // Além disso, essa createTable se preocupa apenas em colocar os dados (caso sejam validados nessa função) no arquivo tables.txt
     createTable(newTableName, resultNameLine, resultTypeLine); // Chama a função createTable para criar a tabela (utils/createTable/createTable.c)
-    lines++;
   }
 
   fclose(heading);
