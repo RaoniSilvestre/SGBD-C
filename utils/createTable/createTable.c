5a4ec5700f8783b24341fbed4e4a77c5897b279d
Raoni Silva <raoni.silvestre@hotmail.com>
2023-11-28 17:46:30 -0300

    Fix file open mode in createTable function

--- utils/createTable/createTable.c
+++ utils/createTable/createTable.c
@@ -6,18 +6,22 @@
 void createTable(char *tableName, char *typeLine, char *nameLine)
 {
   FILE *inputs;
-  inputs = fopen("./heading/tables.txt", "w");
+  inputs = fopen("./heading/tables.txt", "a");
+
   if (inputs == NULL)
   {
     printf("Error opening file!\n");
     return;
   }
+  
   fprintf(inputs, "%s\n", tableName);
   fclose(inputs);
 
   char path[100] = "./tables/";
   strcat(path, tableName);
   strcat(path, ".txt");
+
+
   FILE *table = fopen(path, "w");
   if (table == NULL)
   {
