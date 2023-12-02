4ab866f85788785b648befd739e8436776bd99bc
Raoni Silva <raoni.silvestre@hotmail.com>
2023-12-01 22:59:30 -0300

    Add function headers to main.c

---
+++ functions.h
@@ -0,0 +1,8 @@
+#include "./utils/openMenu/openMenu.h"
+#include "./functions/criarNovaTabela/criarNovaTabela.h"
+#include "./functions/lerTabelas/lerTabelas.h"
+#include "./functions/inserirDados/inserirDados.h"
+#include "./functions/listarDados/listarDados.h"
+#include "./functions/apagarTabela/apagarTabela.h"
+#include "./functions/apagarRegistro/apagarRegistro.h"
+
--- main.c
+++ main.c
@@ -3,15 +3,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-#include "./utils/openMenu/openMenu.h"
-
-#include "./functions/criarNovaTabela/criarNovaTabela.h"
-#include "./functions/lerTabelas/lerTabelas.h"
-#include "./functions/inserirDados/inserirDados.h"
-#include "./functions/listarDados/listarDados.h"
-#include "./functions/apagarTabela/apagarTabela.h"
-#include "./functions/apagarRegistro/apagarRegistro.h"
-
+#include "functions.h"
 
 // A main funcionará como um menu para acessar as funcionalidades do SGBD.
 // Cada funcionalidade será um arquivo .c separado dentro da pasta functions(São funções principais)
