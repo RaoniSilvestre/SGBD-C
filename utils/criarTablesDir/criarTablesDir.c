6aa57a8ab519ff1c6194ea01f6d0941f71fd6b34
Raoni Silva <raoni.silvestre@hotmail.com>
2023-12-04 21:26:14 -0300

    Formating function

--- utils/criarTablesDir/criarTablesDir.c
+++ utils/criarTablesDir/criarTablesDir.c
@@ -1,14 +1,17 @@
 #include "criarTablesDir.h"
 
-void criarTablesDir() {
-    const char* nome_diretorio = "./tables";
-    // Verifica se o diretório existe
-    struct stat st = {0};
-    if (stat(nome_diretorio, &st) == -1) {
-        // Se o diretório não existir, cria-o
-        if (mkdir(nome_diretorio, 0700) != 0) {
-            printf("Erro ao criar o diretório.\n");
-            return;
-        } 
+void criarTablesDir()
+{
+  const char *nome_diretorio = "./tables";
+  // Verifica se o diretório existe
+  struct stat st = {0};
+  if (stat(nome_diretorio, &st) == -1)
+  {
+    // Se o diretório não existir, cria-o
+    if (mkdir(nome_diretorio, 0700) != 0)
+    {
+      printf("Erro ao criar o diretório.\n");
+      return;
     }
+  }
 }
