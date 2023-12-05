484ba20035767796c9fddfc259871f6b30db2902
Raoni Silva <raoni.silvestre@hotmail.com>
2023-12-04 22:07:53 -0300

    Refactor imprimirTabela function

--- functions/listarDados/listarDados.c
+++ functions/listarDados/listarDados.c
@@ -25,32 +25,48 @@ void listarDados()
     strcat(tablePath, tableName);
     strcat(tablePath, ".txt");
 
-    char linha[200];
-    char formatar[40];
-    int qtdLinhas = countLines(tablePath);
-    int qtdColunas = countCommas(tablePath);
+    imprimirTabela(tablePath);
+    system("read -p \"\nPressione ENTER para retornar\" saindo");
+  }
+}
 
-    FILE *table = fopen(tablePath, "r");
-    printf("\n");
-    for (int i = 0; i < qtdLinhas; i++)
+void imprimirTabela(char *tablePath)
+{
+  char linha[200];
+  char formatar[40];
+  int qtdLinhas = countLines(tablePath);
+  int qtdColunas = countCommas(tablePath);
+
+  FILE *table = fopen(tablePath, "r");
+  printf("\n");
+  for (int i = 0; i < qtdLinhas; i++)
+  {
+    fgets(linha, 200, table);
+    if (i == 1)
     {
-      fgets(linha, 200, table);
+      printf("%10s |", "Linha");
+    }
 
+    if (i > 1)
+    {
+      printf("%10d |", i - 1);
+    }
+    if (i >= 1)
+    {
       strcpy(formatar, strtok(linha, ","));
       printf("%10s |", formatar);
-      for (int j = 0; j < qtdColunas+1; j++)
+      for (int j = 0; j < qtdColunas + 1; j++)
       {
         strcpy(formatar, strtok(NULL, ","));
         printf("%10s |", formatar);
-
       }
       strcpy(formatar, strtok(NULL, ","));
       printf("%10s ", formatar);
       printf("\n");
     }
-
-    printf("\n");
-    system("read -p \"\nPressione ENTER para retornar\" saindo");
-    fclose(table);
   }
+
+  printf("\n");
+  
+  fclose(table);
 }
