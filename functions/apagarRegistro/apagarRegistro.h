8721dbb4ff44d32a0585b166ff503f2202dfc089
Raoni Silva <raoni.silvestre@hotmail.com>
2023-12-04 22:07:29 -0300

    Add include for listarDados.h

--- functions/apagarRegistro/apagarRegistro.h
+++ functions/apagarRegistro/apagarRegistro.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../../functions/listarDados/listarDados.h"
+
 #define MAX_FILES 100
 #define MAX_FILENAME_LEN 256
 #define MAX_LINE_LEN 100
