b0486fef5f0eb43dbae500effa6b10ee8e7f194d
Raoni Silva <raoni.silvestre@hotmail.com>
2023-12-13 19:48:41 -0300

    Fix bug in apagarRegistro function

--- functions/apagarRegistro/apagarRegistro.c
+++ functions/apagarRegistro/apagarRegistro.c
@@ -15,54 +15,82 @@ void apagarRegistro()
   if (!verifyTableExists(tableName))
   {
     printf("Tabela nao existe!\n");
+    system("read -p \"\nPressione enter para sair\" saindo");
     return;
   }
-  else
+
+  char tablePath[50] = "./tables/";
+  strcat(tablePath, tableName);
+  strcat(tablePath, ".txt");
+  imprimirTabela(tablePath);
+
+  int lineCount = countLines(tablePath);
+
+  printf("\nNumero de linhas: %d\n", lineCount - 2);
+
+  if (lineCount <= 2)
   {
-    char tablePath[50] = "./tables/";
-    strcat(tablePath, tableName);
-    strcat(tablePath, ".txt");
-    imprimirTabela(tablePath);
+    printf("Nao ha linhas para remover!\n");
+    return;
+  }
 
-    int lineCount = countLines(tablePath);
+  char primaryKey[30];
+  printf("\nDigite a chave primaria do registro que deseja remover: ");
+  scanf("%s", primaryKey);
 
-    printf("\nNumero de linhas: %d\n", lineCount);
+  FILE *file = fopen(tablePath, "r");
+  FILE *temp_file = fopen("./tables/temp.txt", "w");
 
-    if (lineCount <= 2)
+  char line[256];
+  char lineCopy[256];
+  char *field;
+
+  int found = 0;
+
+  while (fgets(line, sizeof(line), file))
+  {
+    strcpy(lineCopy, line);
+    field = strtok(line, ",");
+    if (strcmp(field, primaryKey) == 0)
     {
-      printf("Nao ha linhas para remover!\n");
-      return;
+
+      found = 1;
     }
-
-    char primaryKey[30];
-    printf("\nDigite a chave primaria do registro que deseja remover: ");
-    scanf("%s", primaryKey);
-
-    FILE *file = fopen(tablePath, "r");
-    FILE *temp_file = fopen("./tables/temp.txt", "w");
-
-    char line[256];
-    char lineCopy[256];
-    char *field;
-
-    while (fgets(line, sizeof(line), file))
+    else
     {
-      strcpy(lineCopy, line); 
-      field = strtok(line, ",");
-      if (strcmp(field, primaryKey) != 0)
-      {
-        fputs(lineCopy, temp_file); 
-      }
+      fputs(lineCopy, temp_file);
     }
+  }
 
+  if (!found)
+  {
+    printf("A chave primária %s não foi encontrada!\n", primaryKey);
+    system("read -p \"\nPressione enter para sair\" saindo");
     fclose(file);
     fclose(temp_file);
-
-    remove(tablePath);
-    rename("./tables/temp.txt", tablePath);
-
-    printf("Registro removido com sucesso!\n");
+    remove("./tables/temp.txt");
+    return;
   }
+
+  while (fgets(line, sizeof(line), file))
+  {
+    strcpy(lineCopy, line);
+    field = strtok(line, ",");
+    if (strcmp(field, primaryKey) != 0)
+    {
+      fputs(lineCopy, temp_file);
+    }
+  }
+
+  fclose(file);
+  fclose(temp_file);
+
+  remove(tablePath);
+  rename("./tables/temp.txt", tablePath);
+
+  printf("Registro removido com sucesso!\n");
+
+  system("read -p \"\nPressione enter para sair\" saindo");
 
   return;
 }
